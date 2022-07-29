668e08d0b2dd859386ff86da08571daf8038d724
Armen <harmen24@mail.ru>
2022-07-29 14:32:05 +0400

    Update laplacian_impl.py

--- laplacian_impl.py
+++ laplacian_impl.py
@@ -1,15 +1,17 @@
 import numpy as np
 
 
-def laplacian(a: np.ndarray, b: np.ndarray = np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]]), s: int = 1):
+def laplacian(a: np.ndarray, b: np.ndarray = np.array([[1, 4, 1],
+                                                        [4, -20, 4],
+                                                         [1, 4, 1]]), s: int = 1):
     """Laplacian edge detection"""
-    cols = (a.shape[0] - b.shape[0]) + 1
-    rows = (a.shape[1] - b.shape[0]) + 1
+    cols = (a.shape[0] - b.shape[0]) // s + 1
+    rows = (a.shape[1] - b.shape[0]) // s + 1
 
     lst = np.empty((cols, rows), dtype="int32")
 
-    for i in range(0, cols):
-        for j in range(0, rows):
-            lst[i, j] = (np.sum(a[i: i + b.shape[0], j: j + b.shape[0]] * b))
+    for i in range(0, cols * s, s):
+        for j in range(0, rows * s, s):
+            lst[i // s, j // s] = (np.sum(a[i: i + b.shape[0], j: j + b.shape[0]] * b))
 
     return lst
