7c3de3f432e2facf146840fdea49427ce227c0f5
Armen <harmen24@mail.ru>
2022-07-29 16:32:33 +0400

    change_implementation

--- gause_blure_impl.py
+++ gause_blure_impl.py
@@ -1,15 +1,19 @@
 import numpy as np
 
 
-def gaussian_blur(a: np.ndarray):
+def gaussian_blur(a: np.ndarray, kernel: np.ndarray = np.array([[0.0625, 0.125, 0.0625],
+                                                                [0.125, 0.25, 0.125],
+                                                                [0.0625, 0.125, 0.0625]])):
     """ Remove noise """
-    cols = (a.shape[0] - 3) + 1
-    rows = (a.shape[1] - 3) + 1
+    s = 1
+    cols = (a.shape[0] - kernel.shape[0]) // s + 1
+    rows = (a.shape[1] - kernel.shape[0]) // s + 1
 
     lst = np.empty((cols, rows), dtype="int32")
 
-    for i in range(0, cols):
-        for j in range(0, rows):
-            lst[i, j] = (np.sum(a[i: i + 3, j: j + 3]) // 9)
+    for i in range(0, cols * s, s):
+        for j in range(0, rows * s, s):
+            lst[i // s, j // s] = (np.sum(a[i: i + kernel.shape[0], j: j + kernel.shape[0]] * kernel))
+
 
     return lst
